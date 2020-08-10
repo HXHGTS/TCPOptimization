dfff33095b96aa66f39b7f3e315db21ade84ebdc
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-08-10 11:28:14 +0800

    Create KernelUpdate.sh

---
+++ KernelUpdate.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo "开始升级. . ."
+
+yum -y install epel-release
+
+sed -i "0,/enabled=0/s//enabled=1/" /etc/yum.repos.d/epel.repo
+
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+
+rpm -Uvh https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
+
+yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
+
+echo "正在移除有冲突的旧内核. . ."
+
+yum remove kernel-devel kernel-headers -y
+
+echo "正在安装新内核. . ."
+
+yum --enablerepo=elrepo-kernel install kernel-ml kernel-ml-devel kernel-ml-headers -y
+
+echo "正在将新内核写入开机引导. . ."
+
+sed -i "s/GRUB_DEFAULT=saved/GRUB_DEFAULT=0/" /etc/default/grub
+
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
+echo "正在重启以应用设置. . ."
+
+reboot
+
+echo "正在重启. . ."
