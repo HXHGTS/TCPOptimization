65fb1bf23b03dabf91b9c9ca9bfe89d26e16d652
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-09-01 17:58:18 +0800

    Update TCPO.sh

--- TCPO.sh
+++ TCPO.sh
@@ -22,15 +22,61 @@ echo "* hard memlock unlimited" >> /etc/security/limits.conf
 
 echo "正在开启Google BBR. . ."
 
-sed -e '/net.core.default_qdisc/d;/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf > /etc/sysctl.conf
+rm -rf /etc/sysctl.conf
+
+echo "net.ipv4.ip_forward = 1" > /etc/sysctl.conf
+
+echo "net.ipv6.conf.all.forwarding = 1" >> /etc/sysctl.conf
+
+echo "net.ipv6.conf.default.forwarding = 1" >> /etc/sysctl.conf
+
+echo "net.ipv4.conf.default.rp_filter = 1" >> /etc/sysctl.conf
+
+echo "net.ipv4.conf.default.accept_source_route = 0" >> /etc/sysctl.conf
+
+echo "kernel.core_uses_pid = 1" >> /etc/sysctl.conf
+
+echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf
+
+echo "kernel.msgmnb = 65536" >> /etc/sysctl.conf
+
+echo "kernel.msgmax = 65536" >> /etc/sysctl.conf
+
+echo "net.ipv4.conf.all.promote_secondaries = 1" >> /etc/sysctl.conf
+
+echo "net.ipv4.conf.default.promote_secondaries = 1" >> /etc/sysctl.conf
+
+echo "net.ipv6.neigh.default.gc_thresh3 = 4096" >> /etc/sysctl.conf
+
+echo "net.ipv4.neigh.default.gc_thresh3 = 4096" >> /etc/sysctl.conf
+
+echo "kernel.softlockup_panic = 1" >> /etc/sysctl.conf
+
+echo "kernel.sysrq = 1" >> /etc/sysctl.conf
+
+echo "net.ipv6.conf.all.disable_ipv6 = 0" >> /etc/sysctl.conf
+
+echo "net.ipv6.conf.default.disable_ipv6 = 0" >> /etc/sysctl.conf
+
+echo "net.ipv6.conf.lo.disable_ipv6 = 0" >> /etc/sysctl.conf
+
+echo "kernel.numa_balancing = 0" >> /etc/sysctl.conf
+
+echo "kernel.shmmax = 68719476736" >> /etc/sysctl.conf
+
+echo "kernel.printk = 5" >> /etc/sysctl.conf
 
 echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
-    
+
 echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
 
 sysctl -p
 
-sed -e '/ulimit/d' /etc/rc.d/rc.local > /etc/rc.d/rc.local
+sed -e '/ulimit/d' /etc/rc.d/rc.local > /etc/rc.d/rc.local.temp
+
+cp -rf /etc/rc.d/rc.local.temp /etc/rc.d/rc.local
+
+rm -rf /etc/rc.d/rc.local.temp
 
 echo "ulimit -n 100001" >> /etc/rc.d/rc.local
 
