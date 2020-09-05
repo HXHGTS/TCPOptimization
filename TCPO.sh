bb87fe706fc9936cb5a10372825dd5676ee5c2ef
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-09-06 05:46:49 +0800

    Update TCPO.sh

--- TCPO.sh
+++ TCPO.sh
@@ -6,8 +6,6 @@ yum remove -y $(rpm -qa | grep kernel | grep -v $(uname -r))
 
 echo "正在优化大文件读写性能. . ."
 
-rm -rf /etc/security/limits.conf
-
 echo "* soft nofile 100001" > /etc/security/limits.conf
 
 echo "* hard nofile 100002" >> /etc/security/limits.conf
@@ -22,23 +20,13 @@ echo "* hard memlock unlimited" >> /etc/security/limits.conf
 
 echo "正在开启Google BBR. . ."
 
-sed -e '/net.core.default_qdisc/d;/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf > /etc/sysctl.conf.temp
+sysctl -w net.core.default_qdisc=fq
 
-cp -rf /etc/sysctl.conf.temp /etc/sysctl.conf
-
-rm -rf /etc/sysctl.conf.temp
-
-echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
-
-echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+sysctl -w net.ipv4.tcp_congestion_control=bbr
 
 sysctl -p
 
-sed -e '/ulimit/d' /etc/rc.d/rc.local > /etc/rc.d/rc.local.temp
-
-cp -rf /etc/rc.d/rc.local.temp /etc/rc.d/rc.local
-
-rm -rf /etc/rc.d/rc.local.temp
+sed -i '/ulimit/d' /etc/rc.d/rc.local
 
 echo "ulimit -n 100001" >> /etc/rc.d/rc.local
 
