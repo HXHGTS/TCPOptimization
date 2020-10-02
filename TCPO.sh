09b34c971bf0cf17de8b5436a35494aed28b6481
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-10-02 18:06:18 +0800

    Update TCPO.sh

--- TCPO.sh
+++ TCPO.sh
@@ -6,8 +6,6 @@ yum remove -y $(rpm -qa | grep kernel | grep -v $(uname -r))
 
 echo "正在优化大文件读写性能. . ."
 
-echo. > /etc/security/limits.conf
-
 echo "* soft nofile 512000" > /etc/security/limits.conf
 
 echo "* hard nofile 1024000" >> /etc/security/limits.conf
@@ -22,9 +20,7 @@ echo "* hard memlock unlimited" >> /etc/security/limits.conf
 
 echo "正在开启Google BBR. . ."
 
-echo. > /etc/sysctl.conf
-
-echo 'net.core.default_qdisc=fq' >> /etc/sysctl.conf
+echo 'net.core.default_qdisc=fq' > /etc/sysctl.conf
 
 echo 'net.ipv4.tcp_congestion_control=bbr' >> /etc/sysctl.conf
 
@@ -44,9 +40,7 @@ echo 'net.core.somaxconn=4096' >> /etc/sysctl.conf
 
 sysctl -p
 
-echo. > /etc/rc.d/rc.local
-
-echo "ulimit -n 512000" >> /etc/rc.d/rc.local
+echo "ulimit -n 512000" > /etc/rc.d/rc.local
 
 echo "ulimit -u 512000" >> /etc/rc.d/rc.local
     
