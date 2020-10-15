4c03a8e60364f16a7be5ce5c1141f8cb6ceea6d8
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-10-16 03:12:57 +0800

    Update TCPO.sh

--- TCPO.sh
+++ TCPO.sh
@@ -6,19 +6,7 @@ yum remove -y $(rpm -qa | grep kernel | grep -v $(uname -r))
 
 echo "正在优化大文件读写性能. . ."
 
-echo "root hard nofile 65535" > /etc/security/limits.conf
-
-echo "* soft nofile 65535" >> /etc/security/limits.conf
-
-echo "* hard nofile 65535" >> /etc/security/limits.conf
-
-echo "root soft nofile 65535" >> /etc/security/limits.conf
-
-echo "root hard nofile 65535" >> /etc/security/limits.conf
-
-echo "* soft memlock unlimited" >> /etc/security/limits.conf
-
-echo "* hard memlock unlimited" >> /etc/security/limits.conf
+curl https://raw.githubusercontent.com/HXHGTS/TCPOptimization/master/limits.conf > /etc/security/limits.conf
 
 echo "正在开启Google BBR. . ."
 
