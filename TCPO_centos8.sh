fcfefa6de99f66eded67e1d921a63feb42134710
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-12-26 23:58:30 +0800

    Create TCPO_centos8.sh

---
+++ TCPO_centos8.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "正在移除旧内核. . ."
+
+yum remove -y $(rpm -qa | grep kernel | grep -v $(uname -r))
+
+echo "正在优化大文件读写性能. . ."
+
+curl https://cdn.jsdelivr.net/gh/HXHGTS/TCPOptimization/limits.conf > /etc/security/limits.conf
+
+echo "正在开启Google BBR. . ."
+
+curl https://cdn.jsdelivr.net/gh/HXHGTS/TCPOptimization/sysctl8.conf > /etc/sysctl.conf
+
+sysctl -p
+
+echo "优化完成!"
+
+exit 0
