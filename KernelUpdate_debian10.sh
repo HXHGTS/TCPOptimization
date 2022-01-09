31d33bd048a97b8147bf1b7a9edd3299e32319d4
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-01-09 19:20:37 +0800

    Update KernelUpdate_debian10.sh

--- KernelUpdate_debian10.sh
+++ KernelUpdate_debian10.sh
@@ -10,4 +10,28 @@ apt-get upgrade -y
 
 apt-get dist-upgrade -y
 
-echo 脚本执行完成,请手动重启服务器!
+echo "正在优化大文件读写性能. . ."
+
+curl https://cdn.jsdelivr.net/gh/HXHGTS/TCPOptimization/limits.conf > /etc/security/limits.conf
+
+echo "正在开启Google BBR. . ."
+
+curl https://cdn.jsdelivr.net/gh/HXHGTS/TCPOptimization/sysctl.conf > /etc/sysctl.conf
+
+sysctl -p
+
+echo "正在升级系统软件. . ."
+
+apt install -y build-essential
+
+apt -t bullseye-backports install linux-image-cloud-amd64 -y
+
+apt -t bullseye-backports install linux-headers-cloud-amd64 -y
+
+update-grub
+
+echo 脚本执行完成,正在重启服务器. . .
+
+reboot
+
+
