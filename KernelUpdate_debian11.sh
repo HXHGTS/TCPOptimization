fbe8db024d8449a0e11353ea3e1506649d8e88c7
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-01-09 21:41:01 +0800

    Update KernelUpdate_debian11.sh

--- KernelUpdate_debian11.sh
+++ KernelUpdate_debian11.sh
@@ -4,6 +4,8 @@ echo 正在升级内核. . .
 
 echo "deb http://deb.debian.org/debian bullseye-backports main" >> /etc/apt/sources.list.d/backports.list
 
+apt update
+
 apt -t bullseye-backports install linux-image-cloud-amd64 -y
 
 apt -t bullseye-backports install linux-headers-cloud-amd64 -y
