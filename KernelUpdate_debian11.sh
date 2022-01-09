10a15c6dc750ac6acd883d96467f07c187127b86
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-01-09 16:09:56 +0800

    Create KernelUpdate_debian11.sh

---
+++ KernelUpdate_debian11.sh
@@ -0,0 +1,14 @@
+
+#!/bin/sh
+
+echo 正在升级内核. . .
+
+echo "deb http://deb.debian.org/debian bullseye-backports main" >> /etc/apt/sources.list.d/backports.list
+
+apt-get update -y
+
+apt-get upgrade -y
+
+apt-get dist-upgrade -y
+
+echo 脚本执行完成,请手动重启服务器!
