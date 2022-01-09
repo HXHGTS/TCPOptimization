c6f942af70305ec026770e1862b879e20f94b6cc
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-01-09 19:28:24 +0800

    Update TCPO_debian9.sh

--- TCPO_debian9.sh
+++ TCPO_debian9.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "正在卸载系统软件. . ."
+
+apt-get autoremove -y
+
+apt install -y build-essential
+
 echo "正在优化大文件读写性能. . ."
 
 curl https://cdn.jsdelivr.net/gh/HXHGTS/TCPOptimization/limits.conf > /etc/security/limits.conf
@@ -9,18 +15,6 @@ echo "正在开启Google BBR. . ."
 curl https://cdn.jsdelivr.net/gh/HXHGTS/TCPOptimization/sysctl.conf > /etc/sysctl.conf
 
 sysctl -p
-
-echo "正在升级系统软件. . ."
-
-apt update -y
-
-apt install -y build-essential
-
-apt -t stretch-backports install linux-image-cloud-amd64
-
-apt -t stretch-backports install linux-headers-cloud-amd64
-
-update-grub
 
 echo "优化完成!"
 
