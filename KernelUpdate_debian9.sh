52b9f385d8e4b4ec0591cfec256379012a03eb93
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-01-09 19:27:47 +0800

    Update KernelUpdate_debian9.sh

--- KernelUpdate_debian9.sh
+++ KernelUpdate_debian9.sh
@@ -2,7 +2,7 @@
 
 echo 正在升级内核. . .
 
-echo "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/backports.list
+echo "deb http://deb.debian.org/debian stretch-backports main" >> /etc/apt/sources.list.d/backports.list
 
 apt-get update -y
 
@@ -10,4 +10,14 @@ apt-get upgrade -y
 
 apt-get dist-upgrade -y
 
-echo 脚本执行完成,请手动重启服务器!
+echo "正在升级系统软件. . ."
+
+apt -t stretch-backports install linux-image-cloud-amd64 -y
+
+apt -t stretch-backports install linux-headers-cloud-amd64 -y
+
+update-grub
+
+echo 脚本执行完成,正在重启服务器. . .
+
+reboot
