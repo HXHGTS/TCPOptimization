2d771dddd8b70bfeacc9c1f2caa2040c84f377d5
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-02-07 11:53:45 +0800

    Update KernelUpdate_debian10.sh

--- KernelUpdate_debian10.sh
+++ KernelUpdate_debian10.sh
@@ -2,7 +2,7 @@
 
 echo 正在升级内核. . .
 
-echo "deb http://deb.debian.org/debian buster-backports-sloppy main" > /etc/apt/sources.list.d/backports.list
+echo "deb http://deb.debian.org/debian buster-backports-sloppy main" >> /etc/apt/sources.list.d/backports.list
 
 apt -t buster-backports install linux-image-cloud-amd64 -y
 
