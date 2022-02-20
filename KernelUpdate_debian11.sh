2114d68749e09c786be4ec3a37e1f2f5feada35d
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-02-20 11:52:37 +0800

    Update KernelUpdate_debian11.sh

--- KernelUpdate_debian11.sh
+++ KernelUpdate_debian11.sh
@@ -2,7 +2,7 @@
 
 echo 正在升级内核. . .
 
-echo "deb http://deb.debian.org/debian bullseye-backports main" >> /etc/apt/sources.list.d/backports.list
+echo "deb http://deb.debian.org/debian bullseye-backports main" > /etc/apt/sources.list.d/backports.list
 
 apt update
 
